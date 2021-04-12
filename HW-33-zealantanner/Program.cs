4d9b996b6e4ea55d8c7319e0982b17918f49e43e
zealantanner <77339919+zealantanner@users.noreply.github.com>
2021-04-12 05:23:57 -0600

    Add files via upload

---
+++ HW-33-zealantanner/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    private static void Print(IEnumerable<KeyValuePair<string, ConsoleColor>> items)
+    {
+        foreach (KeyValuePair<string, ConsoleColor> item in items)
+        {
+            Console.ForegroundColor = item.Value;
+            Console.Write(item.Key);
+            Console.ResetColor();
+        }
+        Console.WriteLine();
+    }
+    private static void BackPrint(IEnumerable<KeyValuePair<string, ConsoleColor>> items)
+    {
+        foreach (KeyValuePair<string, ConsoleColor> item in items)
+        {
+            Console.ForegroundColor = ConsoleColor.Black;
+            Console.BackgroundColor = item.Value;
+            Console.Write(item.Key);
+            Console.ResetColor();
+        }
+        Console.WriteLine();
+    }
+    static void Main()
+    {
+        var textcolorMap = new Dictionary<string, ConsoleColor>
+        {
+            ["Error"] = ConsoleColor.Red,
+            ["Warning"] = ConsoleColor.Yellow,
+            ["Information"] = ConsoleColor.Green
+        };
+        textcolorMap.Add("Verbose", ConsoleColor.White);
+        textcolorMap["Error"] = ConsoleColor.Cyan;
+
+        BackPrint(textcolorMap);
+        Print(textcolorMap);
+    }
+}
+
+interface IPair<T>
+{
+    T First { get; }
+    T Second { get; }
+    T this[PairItem index] { get; }
+}
+
+public enum PairItem
+{ First, Second }
+public struct Pair<T> : IPair<T>
+{
+    public Pair(T first, T second)
+    {
+        First = first;
+        Second = second;
+    }
+    public T First { get; }
+    public T Second { get; }
+    public T this[PairItem index]
+    {
+        get
+        {
+            switch (index)
+            {
+                case PairItem.First: return First;
+                case PairItem.Second: return Second;
+                default: throw new NotImplementedException($"The enum {index.ToString() }has not been implemented");
+            }
+        }
+    }
+}
